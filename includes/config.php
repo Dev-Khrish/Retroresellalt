cf8b3f8c06b6711fbc3f78bbdaaf299152e9cc3a
Dev-Khrish <spidykhrishop007@gmail.com>
2025-04-14 17:57:56 +0530

    first commit

--- includes/config.php
+++ includes/config.php
@@ -2,10 +2,10 @@
 <?php
 //making config as we need this everytime we can just use it through include_once
 //1st step for database php connection
-$serverName = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "db_ecommerce";
+$serverName = "sql12.freesqldatabase.com";
+$dBUsername = "sql12773057";
+$dBPassword = "MFfe9Mp89l";
+$dBName = "sql12773057";
 
 //Before we can access data in the MySQL database, we need to be able to connect to the server i.e php
 $conn = new mysqli($serverName,$dBUsername,$dBPassword,$dBName );
